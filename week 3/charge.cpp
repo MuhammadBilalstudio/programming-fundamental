6e11e44888b667e9455649b0fb941d3ebec6bb7f
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-02-28 19:40:02 +0500

    Charge Calculation for time and current done

---
+++ week 3/charge.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float charge, current, time;
+
+    cout << "Enter the charge: ";
+    cin >> current;
+
+    cout << "Enter the time: ";
+    cin >> time;
+
+    charge = current * time;
+
+    cout << "The charge is: " << charge << endl;
+
+    return 0;
+}
