294a123a005b3c0ea5828f1ce4dc50fd5e237e26
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-03-15 20:14:48 +0500

    Password logic

---
+++ week4/inputpass.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string my_password, user_enter;
+
+    my_password = "Pass@123!";
+    cout << "Enter the password: ";
+    cin >> user_enter;
+
+    if (user_enter == my_password) {
+        cout << "Wow! You have cracked the code!";
+    }
+    else {
+        cout << "It is not that simple, Try Again";
+    }
+
+    return 0;
+}
