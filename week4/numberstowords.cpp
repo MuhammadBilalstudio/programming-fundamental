ed94f2f4e34a3ca5da1e543852979ce8e1ff822c
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-03-15 23:48:44 +0500

    human readable figures to words if else logic

---
+++ week4/numberstowords.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number (0-100): ";
+    cin >> num;
+
+    if (num < 0 || num > 100) {
+        cout << "Out of range" << endl;
+        return 0;
+    }
+
+    if (num == 100) { cout << "one hundred" << endl; }
+    else if (num == 0) { cout << "zero" << endl; }
+    else if (num >= 20) {
+        // Handle Tens
+        int t = num / 10;
+        if (t == 2) cout << "twenty ";
+        else if (t == 3) cout << "thirty ";
+        else if (t == 4) cout << "forty ";
+        else if (t == 5) cout << "fifty ";
+        else if (t == 6) cout << "sixty ";
+        else if (t == 7) cout << "seventy ";
+        else if (t == 8) cout << "eighty ";
+        else if (t == 9) cout << "ninety ";
+
+        // Handle Ones
+        int o = num % 10;
+        if (o == 1) cout << "one";
+        else if (o == 2) cout << "two";
+        else if (o == 3) cout << "three";
+        else if (o == 4) cout << "four";
+        else if (o == 5) cout << "five";
+        else if (o == 6) cout << "six";
+        else if (o == 7) cout << "seven";
+        else if (o == 8) cout << "eight";
+        else if (o == 9) cout << "nine";
+        cout << endl;
+    } 
+    else if (num >= 10) {
+        // Handle Teens
+        if (num == 10) cout << "ten";
+        else if (num == 11) cout << "eleven";
+        else if (num == 12) cout << "twelve";
+        else if (num == 13) cout << "thirteen";
+        else if (num == 14) cout << "fourteen";
+        else if (num == 15) cout << "fifteen";
+        else if (num == 16) cout << "sixteen";
+        else if (num == 17) cout << "seventeen";
+        else if (num == 18) cout << "eighteen";
+        else if (num == 19) cout << "nineteen";
+        cout << endl;
+    } 
+    else {
+        // Handle single digits
+        if (num == 1) cout << "one";
+        else if (num == 2) cout << "two";
+        else if (num == 3) cout << "three";
+        else if (num == 4) cout << "four";
+        else if (num == 5) cout << "five";
+        else if (num == 6) cout << "six";
+        else if (num == 7) cout << "seven";
+        else if (num == 8) cout << "eight";
+        else if (num == 9) cout << "nine";
+        cout << endl;
+    }
+
+    return 0;
+}
