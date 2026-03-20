1cdab15965317529d8c66caf7dcf7d8103d9f434
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-03-20 20:17:00 +0500

    Counting 1 to 10 using a while\for loop

---
+++ week 5/for/whilecounting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i = i + 1) {
+        cout << i << endl;
+    }
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 10) {
+        cout << i << endl;
+        i = i + 1;
+    }
+    return 0;
+}
