d7f9fc46c67197b1f43515db3c817f36558a8b8a
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-03-21 21:29:23 +0500

    Fibonacci 2.0 sequence generator using a while loop

---
+++ week 5/fibonacci2.0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void generateFibonacci(int n) {
+    int a = 0, b = 1, next;
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    generateFibonacci(n);
+    return 0;
+}
