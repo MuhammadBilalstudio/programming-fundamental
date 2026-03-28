41cdbbe028e7f34ff3dd56cad7099ab9194941cf
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-03-28 21:40:40 +0500

    Add array of five element task

---
+++ week 6/arrayoffiveelement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int values[5];
+
+    values[0] = 10;
+    values[1] = 20;
+    values[2] = 30;
+    values[3] = 40;
+    values[4] = 50;
+
+    cout << "The 2nd element is: " << values[1] << endl;
+    cout << "The 4th element is: " << values[3] << endl;
+
+    return 0;
+}
