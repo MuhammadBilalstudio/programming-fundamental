5c9fd8972a785532f9cf1eb63cfebf9c0f6f43f6
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-03-28 21:36:59 +0500

    Add Task 1 arrays integers

---
+++ week 6/task1_arraysintegers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5];
+
+    marks[0] = 85;
+    marks[1] = 92;
+    marks[2] = 78;
+    marks[3] = 90;
+    marks[4] = 88;
+
+    cout << "Value at index 0: " << marks[0] << endl;
+    cout << "Value at index 1: " << marks[1] << endl;
+    cout << "Value at index 2: " << marks[2] << endl;
+    cout << "Value at index 3: " << marks[3] << endl;
+    cout << "Value at index 4: " << marks[4] << endl;
+
+    return 0;
+}
