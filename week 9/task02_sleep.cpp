071058d2903889f1e7d60db21dddb7ef7444199d
MuhammadBilalstudio <tayyabbilal784@gmail.com>
2026-04-19 21:31:25 +0500

    Use pre-defined Sleep function from windows.h to halt execution for 200ms

---
+++ week 9/task02_sleep.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() 
+{
+    cout << "Execution started..." << endl;
+
+    Sleep(200);
+
+    cout << "Execution resumed after 200 milliseconds." << endl;
+
+    return 0;
+}
